66e7bf446049d388990029799463f4b44570f249
oriphiel-hr <Dobri1Duh2Git3>
2025-09-22 23:10:41 +0200

    Uslugar - podizanje projekta

--- uslugar/server.js
+++
@@ -1,3 +0,0 @@
-const e=require("express")(); const p=process.env.PORT||80;
-e.get("/health",(_,r)=>r.send("ok")); e.get("/",(_,r)=>r.send("nav ok"));
-e.listen(p,"0.0.0.0",()=>console.log("nav on",p));
